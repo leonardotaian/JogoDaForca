c4e27e83ad25b8e15df86c654f0827ad075daaf6
Leonardo Taian <leonardotaianav@outlook.com>
2023-10-17 23:11:27 -0300

    trocando funções

--- main.py
+++ main.py
@@ -1,43 +1,29 @@
-from math import isnan
 from dicionario import dic
-import random, time
+import random
 
-
-
-repetidas = ["init"]
-
+repetidas = []
 aleat = random.randint(0, 100)
-
-palavra = (dic[aleat])
+palavra = dic[aleat].strip()
 quant_letras = len(palavra)
 
 print("Que os jogos comecem...")
+print(f"\nA palavra a qual você terá que descobrir tem {quant_letras} letras.")
+print(f"Você terá exatamente {quant_letras} chances para descobrir a palavra. Boa sorte!\n")
 
-print("")
-print("A palavra a qual você terá que descobrir tem",quant_letras,"letras")
-print("Você terá exatamente",quant_letras,"chances para descobrir, acredito que seja o suficiente.")
-print("Boa sorte!")
+for i in range(quant_letras):
+    tentativa = input("Tente uma letra: ").strip().lower()
+    if tentativa in repetidas:
+        print("Letra Repetida")
+        
+    elif len(tentativa) != 1 or not tentativa.isalpha():
+        print("Tá tentando dar o golpe? Apenas uma letra e tem que ser válida.")
+        
+    else:
+        repetidas.append(tentativa)
+        if tentativa in palavra:
+            print("Acertou!")
+        else:
+            print("Furada.")
 
-x = quant_letras
-
-while x >= 0:
-
-  tentativa = str(input("Tente uma letra: "))
-  repetidas.append(tentativa)
-  numero = any(char.isdigit() for char in tentativa)
-  
-  if repetidas.count(tentativa) == 1:
-      print("Letra Repetida")
-      repetidas.pop()
-      x = x + 1
-  elif len(tentativa) >= 2:
-    print("Olha o golpe KKKKKKK é só uma letra!")
-    repetidas.pop()
-    x = x + 1
-  elif bool(numero) == True:
-    print("Tá achando que tá num bingo? Digite uma letra")
-    repetidas.pop()
-    x = x + 1
-  else:
-    x = x - 1
+print("\nFim do jogo. A palavra era:", palavra)
 
